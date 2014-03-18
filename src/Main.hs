e04104a0ac94d89bddfe702e405282ee7bb72026
Hannes Gräuler <hgraeule@uos.de>
2014-03-18 21:35:45 +0100

    Enable default 2048 mode (Sum monoid)

--- src/Main.hs
+++ src/Main.hs
@@ -2,7 +2,7 @@ import qualified HS2048
 import Data.Monoid
 
 main :: IO ()
---main = HS2048.main (Sum 2)
+main = HS2048.main (Sum 2)
 --main = HS2048.main (Product 2)
 --main = HS2048.main "!"
 --main = HS2048.main [42]
